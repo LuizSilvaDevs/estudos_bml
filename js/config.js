c5e64982ced5a7ad13821e6dacc13623e6d14f0d
Luiz Davi <luizdrsilva.dev@gmail.com>
2025-01-16 11:10:45 -0300

    fodasse

--- js/config.js
+++ js/config.js
@@ -6,12 +6,12 @@ const users = [
       username: 'dasilva',
       password: 'dasilva'
   },
-  {
-      username: 'cabosblm',
-      password: 'cabosblm'
-  },
-  {
-    username: 'cfc2025',
-    password: 'cfc2025'
-  }
+  // {
+  //     username: 'cabosblm',
+  //     password: 'cabosblm'
+  // },
+  // {
+  //   username: 'cfc2025',
+  //   password: 'cfc2025'
+  // }
 ];
